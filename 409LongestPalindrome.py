867eef4a2e5a537c032aafb336b53eb77ca768b4
javin <1059720403@qq.com>
2021-12-29 14:31:47 +0800

    LeetCode409最长回文串

---
+++ 409LongestPalindrome.py
@@ -0,0 +1,22 @@
+# 最长回文串
+def longestPalindrome(s):
+    dict = {}
+    for x in s:
+        if x not in dict:
+            dict[x] = 1
+        else:
+            dict[x] = dict[x] + 1
+    else:
+        odd = 0
+        even = 0
+        for value in dict.values():
+            if value % 2 != 0:
+                odd = odd + 1
+                even = 1
+        return len(s) - odd + even
+
+
+# s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+s = "abccccdd"
+print(longestPalindrome(s))
+
