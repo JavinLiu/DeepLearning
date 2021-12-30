1fb773542ebfd237723f706dd8aa602bd9fa9dbc
javin <1059720403@qq.com>
2021-12-30 17:59:14 +0800

    LeetCode0583两个字符串的删除操作

--- 0583DeleteOperationforTwoStrings.py
+++ 0583DeleteOperationforTwoStrings.py
@@ -1,5 +1,5 @@
 # 两个字符串的删除操作
-
+# dp，自底向上，迭代
 def minDistance(word1, word2):
     n = len(word1)
     m = len(word2)
@@ -17,7 +17,33 @@ def minDistance(word1, word2):
     return dp[m][n]
 
 
-word1 = "sea"
-word2 = "eat"
+# 带备忘录的递归，自顶向下
+def minDistance1(word1, word2):
+    # dp(word1, word2, i, j)表示word1[i...n], word2[j...m]的最少删除解
+    n = len(word1)
+    m = len(word2)
+    mem = [[-1 for _ in range(n)] for _ in range(m)]
+
+    def dp(word1, word2, i, j):
+        if i == n:
+            return m - j
+        if j == m:
+            return n - i
+        if mem[j][i] != -1:
+            return mem[j][i]
+        else:
+            if word1[i] == word2[j]:
+                mem[j][i] = dp(word1, word2, i + 1, j + 1)
+            else:
+                mem[j][i] = min(dp(word1, word2, i + 1, j), dp(word1, word2, i, j + 1)) + 1
+            return mem[j][i]
+
+    dp(word1, word2, 0, 0)
+    return mem[0][0]
+
+
+word1 = "seed"
+word2 = "seat"
 print(minDistance(word1, word2))
+print(minDistance1(word1, word2))
 
